59a79685224af353535eb41644ea15fa83f6995f
Alberto Monaco <alberto.monaco84@gmail.com>
2025-03-05 14:35:47 +0100

    complete exercise

--- main.js
+++ main.js
@@ -19,3 +19,21 @@
 
 //! Esempio di output atteso
 // Data di nascita dello chef: 1990-06-15
+
+async function getChefBirthday(id) {
+	const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`)
+	const recipe = await recipeResponse.json()
+	console.log(recipe)
+	const chefResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
+	const chef = await chefResponse.json()
+	console.log(chef)
+	return chef.birthDate
+}
+;(async () => {
+	try {
+		const birthday = await getChefBirthday(1)
+		console.log('Data di nascita dello chef:', birthday)
+	} catch (error) {
+		console.error('Errore:', error.message)
+	}
+})()
